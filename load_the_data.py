25b0cfaf8214cdeea88e5d9d91b5a365d0023eec
senedm <43731141+senedm@users.noreply.github.com>
2018-10-13 01:19:24 +0200

    Update load_the_data.py

--- load_the_data.py
+++ load_the_data.py
@@ -6,29 +6,31 @@ import numpy as np
 root_path = "/home/sened/Dokumentumok/Omixon_entry_questions"
 file_name = 'test2.txt'
 
-data = os.path.join(root_path, file_name)
-check = []
-
-RNA = ['A','C','G','U']
-DNA = ['A','C','G','T']
-
-
-with open(data) as infile:
-    for line in infile:
-        check.append(line)
-
-if '\n' in check:
-    print('WARNING: The analysis writing could interrupted, it contains new rows')
-
+def load_the_data(root_path, file_name):
     
-for i in range(len(check[0])-1):
-    if check[0][i] in RNA or check[0][i] in DNA:
-        continue
-    else:
-        print('')
-        print('ERROR: ' + check[0][i] + ' is NOT a base ' + '#' + str(i))
-        print('')
+    data = os.path.join(root_path, file_name)
+    check = []
+
+    RNA = ['A','C','G','U']
+    DNA = ['A','C','G','T']
 
 
-    
-    
+    with open(data) as infile:
+        for line in infile:
+            check.append(line)
+
+    if '\n' in check:
+        print('WARNING: The analysis writing could interrupted, it contains new rows')
+
+
+    for i in range(len(check[0])-1):
+        if check[0][i] in RNA or check[0][i] in DNA:
+            continue
+        else:
+            print('')
+            print('ERROR: ' + check[0][i] + ' is NOT a base ' + '#' + str(i))
+            print('')
+            
+    return check
+
+test1 = load_the_data(root_path, file_name)
