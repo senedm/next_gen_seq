866abafcd00fc9674c057481112ae5363a6595a2
senedm <43731141+senedm@users.noreply.github.com>
2018-10-13 01:15:16 +0200

    Create load_the_data.py

---
+++ load_the_data.py
@@ -0,0 +1,34 @@
+#this function opens the unknown seqence from .txt format and the checks it
+
+import os
+import numpy as np
+
+root_path = "/home/sened/Dokumentumok/Omixon_entry_questions"
+file_name = 'test2.txt'
+
+data = os.path.join(root_path, file_name)
+check = []
+
+RNA = ['A','C','G','U']
+DNA = ['A','C','G','T']
+
+
+with open(data) as infile:
+    for line in infile:
+        check.append(line)
+
+if '\n' in check:
+    print('WARNING: The analysis writing could interrupted, it contains new rows')
+
+    
+for i in range(len(check[0])-1):
+    if check[0][i] in RNA or check[0][i] in DNA:
+        continue
+    else:
+        print('')
+        print('ERROR: ' + check[0][i] + ' is NOT a base ' + '#' + str(i))
+        print('')
+
+
+    
+    
